b442fa2dc52063ae826cfade2bb2b6784554f119
sammy wlodawski <samwlodawski@gmail.com>
2024-07-17 18:11:24 -0400

    Adding comments for my get and post routes.

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
+// I am bringing in the express library and creating a route
 const Task = require('../../models/Task');
-
+// I am bringing in the task file from the model folder..
 router.get('/', async(req, res) => {
     try {
         const allTasks = await Task.findall();
@@ -10,13 +11,15 @@ router.get('/', async(req, res) => {
 
      }  
 })
-
+// I am setting a get route to await a request to retrieve and then cycle through the task array and return a succesful
+// status code and convert the data to ajson object if no issues when executing otherwise catch and return a 500 error.
 router.post('/', async(req, res) => {
     try {
         const newTask = await Task.create({
             name: req.body.name,
             description: req.body.description,
             status: req.body.status
+// I am setting a post route to snatch the values from the user to allow them to create a new task w name, description and status. Then update the server side data.
         })
         if(newTask) {
             res.status(200).json(newTask);
@@ -25,5 +28,6 @@ router.post('/', async(req, res) => {
             res.status(500).json(err);
         }
     })
-
+// I am passing the newly created task as a parameter and setting an error check. If successful return a 200 status otherwise catch and return a 500 status error.
     module.exports = router;
+// I am exporting the get and post routes to be used in my application.
